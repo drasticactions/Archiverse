c85b53825657983fc791449b8a71b03a1cfe0614
Tim Miller <innerlogic4321@gmail.com>
2018-12-01 20:37:08 -0500

    Add Offline message, enable PWA

--- Miiworse/ClientApp/src/components/Layout.tsx
+++ Miiworse/ClientApp/src/components/Layout.tsx
@@ -16,15 +16,31 @@ class Layout extends React.Component {
             isOpen: !this.state.isOpen
         });
     }
+
+
+
     render() {
-        return (
-            <div>
-                <NavMenu />
-                <main role="main">
-                    {this.props.children}
-                </main>
+        if (navigator.onLine) {
+            return (
+                <div>
+                    <NavMenu />
+                    <main role="main">
+                        {this.props.children}
+                    </main>
+                </div>
+            );
+        }
+        else {
+            return <div className="loader">
+                <div className="center-block">
+                    <img className="center-block img-responsive" src="/img/archiverse-logo.png" />
+                </div>
+                <div className="center-block">
+                    <h2 className="text-center">In order to explore the Archiverse, you must be connected to the Internet!</h2>
+                    <h3 className="text-center">Verify your Internet connection and refresh.</h3>
+                </div>
             </div>
-        );
+        }
     }
 }
 
